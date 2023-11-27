d684006dbe01601ceb1fc472c5d0e48ad4121e46
Andrey Picado Arias <50878739+Robotooo@users.noreply.github.com>
2023-11-26 22:04:44 -0600

    EventCard

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,7 +1,8 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView,} from 'react-native';
 import { TAppLayoutProps } from '../layouts/AppLayout';
+import EventCard from '../components/Cards/EventCard';
 
 type THomeProps = NativeStackScreenProps<TAppLayoutProps>;
 
@@ -12,14 +13,43 @@ const Home: React.FC<THomeProps> = ({ navigation }) => {
       flex: 1,
       alignItems: 'center',
       justifyContent: 'center',
+      
     }}
     >
-      <Text
-      style={{
-        fontSize: 30,
-        fontWeight: 'bold',
-      }}
-      >Home</Text>
+      <ScrollView>
+        <EventCard 
+          activityName='Weler perico'
+          dateTime='27/11/2023'
+          description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut accumsan eros nec arcu congue, vel suscipit nulla ornare. Proin varius sapien ac leo viverra, in fringilla urna eleifend. Suspendisse non mi non quam eleifend vestibulum vitae ac ipsum.'
+          image={require('/assets/img.jpg')}
+        />
+        <EventCard 
+          activityName='Tomar waro'
+          dateTime='29/11/2023'
+          description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut accumsan eros nec arcu congue, vel suscipit nulla ornare. Proin varius sapien ac leo viverra, in fringilla urna eleifend. Suspendisse non mi non quam eleifend vestibulum vitae ac ipsum.'
+          image={require('/assets/img.jpg')}
+        />
+        <EventCard 
+          activityName='Fumar Mariwanas'
+          dateTime='29/11/2023'
+          description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut accumsan eros nec arcu congue, vel suscipit nulla ornare. Proin varius sapien ac leo viverra, in fringilla urna eleifend. Suspendisse non mi non quam eleifend vestibulum vitae ac ipsum.'
+          image={require('/assets/img.jpg')}
+        />
+        <EventCard 
+          activityName='Tomar waro'
+          dateTime='29/11/2023'
+          description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut accumsan eros nec arcu congue, vel suscipit nulla ornare. Proin varius sapien ac leo viverra, in fringilla urna eleifend. Suspendisse non mi non quam eleifend vestibulum vitae ac ipsum.'
+          image={require('/assets/img.jpg')}
+        />
+        <EventCard 
+          activityName='Tomar waro'
+          dateTime='29/11/2023'
+          description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut accumsan eros nec arcu congue, vel suscipit nulla ornare. Proin varius sapien ac leo viverra, in fringilla urna eleifend. Suspendisse non mi non quam eleifend vestibulum vitae ac ipsum.'
+          image={require('/assets/img.jpg')}
+        />
+      </ScrollView>
+      
+
     </View>
   );
 };
